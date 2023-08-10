f56705d5c6453317cc2d0fb7d872a65eeea4d438
saiful_ <saiful@amhactech.com>
2023-08-10 14:37:11 +0600

    take user input to sort an array(selection sort)

--- selectionsort.js
+++ selectionsort.js
@@ -1,15 +1,45 @@
 
+// function selection_sort(arr) {
+//     for (let i = 0; i < arr.length - 1; i++) {
+//         var index_min = i;
+
+//         for (let j = i + 1; j < arr.length; j++) { 
+//             if (arr[index_min] > arr[j]) {
+//                  index_min = j;
+//             }
+//         }
+
+//         if (index_min !== i) { 
+//             let temp = arr[i];
+//             arr[i] = arr[index_min];
+//             arr[index_min] = temp;
+//         }
+//     }
+//     console.log({ arr });
+// }
+
+// selection_sort([12, 43, 1, 5, 234]);
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 function selection_sort(arr) {
     for (let i = 0; i < arr.length - 1; i++) {
         var index_min = i;
 
-        for (let j = i + 1; j < arr.length; j++) { 
+        // Find the index of the minimum element in the unsorted part of the array
+        for (let j = i + 1; j < arr.length; j++) {
             if (arr[index_min] > arr[j]) {
-                 index_min = j;
+                index_min = j;
             }
         }
 
-        if (index_min !== i) { 
+        // Swap the minimum element with the first element in the unsorted part
+        if (index_min !== i) {
             let temp = arr[i];
             arr[i] = arr[index_min];
             arr[index_min] = temp;
@@ -18,4 +48,9 @@ function selection_sort(arr) {
     console.log({ arr });
 }
 
-selection_sort([12, 43, 1, 5, 234]);
+// Read user input and call the sorting function
+rl.question('Enter numbers separated by spaces: ', (input) => {
+    const arr = input.split(' ').map(Number);
+    selection_sort(arr);
+    rl.close();
+});
